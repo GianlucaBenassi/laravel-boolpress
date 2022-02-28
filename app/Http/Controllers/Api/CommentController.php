8d219ff49cc9ef60bcbaf87f8a2eb338cb981a3b
Gianluca Benassi <benox1199@gmail.com>
2022-02-28 14:33:09 +0100

    Comment controller

---
+++ app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function store(Request $request)
+    {
+        // validation
+
+        // store data
+
+        // response
+    }
+}
